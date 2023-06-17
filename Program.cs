1c4b918e818481a4404ef90f615afcd5a6d123a1
Mitja <mituuuuz@hotmail.com>
2023-06-17 10:30:40 +0300

    Add a basic implementation of the database. Create a simple insert and query program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using LiteDB;
+
+namespace Application {
+    class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+
+            using(var db = new LiteDatabase(@"/home/mituuz/.liteDb")) {
+                var collection = db.GetCollection<PetWeight>("petweights");
+
+                var petWeight = new PetWeight (
+                    "Pet",
+                    1.5,
+                    DateTime.Now
+                );
+
+                collection.Insert(petWeight);
+
+                collection.EnsureIndex(x => x.Name);
+
+                var results = collection.Query()
+                    .Where(x => x.Name.StartsWith("P"))
+                    .ToList();
+
+                foreach (var weight in results)
+                {
+                    Console.WriteLine(weight.Name);
+                }
+            }
+        }
+    }
+
+    class PetWeight {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public Double Weight { get; set; }
+        public DateTime Date { get; set; }
+
+        public PetWeight(String Name, Double Weight, DateTime Date) {
+            this.Name = Name;
+            this.Weight = Weight;
+            this.Date = Date;
+        }
+    }
+}
