acb229da68f377267139a4d24f0a952cd2a28e57
Mitja Leino <mitja.leino@hotmail.com>
2023-11-09 07:05:46 +0200

    Add sln file. Rename appsettings

--- app/Program.cs
+++ app/Program.cs
@@ -1,32 +1,28 @@
-﻿using System;
-using System.Net;
+﻿using HttpServer;
 using LiteDB;
-using HttpServer;
-using Models;
 using Microsoft.Extensions.Configuration;
 
-namespace Application {
-    class Program {
+namespace RasLiteDB {
+    internal static class Program {
         public static async Task Main(string[] args) {
             var configurationBuilder = new ConfigurationBuilder();
-            configurationBuilder.AddJsonFile("appsettings.json");
-            configurationBuilder.AddJsonFile("appsettings.Development.json", optional: true);
-            var configuration = configurationBuilder.Build();
+            configurationBuilder.AddJsonFile("appSettings.json");
+            configurationBuilder.AddJsonFile("appSettings.Development.json", optional: true);
+            IConfigurationRoot configuration = configurationBuilder.Build();
 
             string? dbPath = configuration["RasLiteSettings:DbPath"];
             string? raspIp = configuration["RasLiteSettings:RaspIp"];
 
             if (raspIp == null) {
-                Console.WriteLine("No IP defined in appsettings.json!");
+                Console.WriteLine("No IP defined in appSettings.json!");
                 return;
             }
 
             // Creates the database if it doesn't exist
-            using(var db = new LiteDatabase(dbPath)) {
-                SimpleHttpServer server = new SimpleHttpServer(raspIp, db);
+            using var db = new LiteDatabase(dbPath);
+            var server = new SimpleHttpServer(raspIp, db);
 
-                await server.StartListeningAsync();
-            }
+            await server.StartListeningAsync();
         }
     }
 }
